e657259d6d6e283fa10f153a18b963c4826a2bff
crisantt <jcsanjose@proton.me>
2025-10-18 08:33:26 +0000

    edited main

--- hello/main.go
+++ hello/main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"fmt" // This is a standard library format
 	"example/greet" // This go file a module.
+	"fmt"           // This is a standard library format
 )
 
 func main() {
-	sum := greet.Add(10,10,10)
+	sum := greet.Add(10, 10, 10)
 	fmt.Println(sum)
 	mensahe := greet.Hello("Ninja")
 	fmt.Println(mensahe)
